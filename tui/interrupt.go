cd858c8df4020cf485fa52d53870a83b22154db2
AJ Alon <aj.alon@datarobot.com>
2026-01-13 18:49:36 -0800

    fixup! log ctrl+c

--- tui/interrupt.go
+++ tui/interrupt.go
@@ -35,7 +35,7 @@ func (m InterruptibleModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	if keyMsg, ok := msg.(tea.KeyMsg); ok {
 		if keyMsg.String() == "ctrl+c" {
 			// Log the interrupt for debugging purposes
-			log.Debug("InterruptibleModel: Ctrl-C detected, quitting...")
+			log.Info("Ctrl-C detected, quitting...")
 			return m, tea.Quit
 		}
 	}
